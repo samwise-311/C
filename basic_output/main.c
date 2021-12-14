af6c4952bee9f001fde5102aa5f56daa629cd3ce
Sam W <61138994+samwise-311@users.noreply.github.com>
2021-12-14 11:50:54 -0700

    Add files via upload

---
+++ basic_output/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+   // Draw tree
+   printf("   *\n");
+   printf("  ***\n");
+   printf(" *****\n");
+   printf("*******\n");
+   printf("  ***\n\n");
+   printf("/\\   /\\\n");
+   printf("  o o\n");
+   printf(" =   =\n");
+   printf("  ---\n");
+   return 0;
+}
